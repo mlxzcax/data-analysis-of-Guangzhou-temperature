12d7801dba2915cc47c98c44af51874e47686251
mlxzcax <38780675+mlxzcax@users.noreply.github.com>
2019-09-09 17:15:12 +0800

    Update graph.py

--- graph.py
+++ graph.py
@@ -7,27 +7,28 @@ count=0
 high=[]
 low=[]
 
-
+#读取文件，得到气温列
 s='3月天气.csv'
 with open(s,'r',encoding="utf-8") as csvfile:
     reader = csv.DictReader(csvfile)
     column1 = [row["气温"] for row in reader]
-#print(len(column1))
+#把高低气温抽出，放到两个数组中
 for x in column1:
     temp=re.findall(r"\d+\.?\d*",x)
     high.append(int(temp[0]))
     low.append(int(temp[1]))
     count+=1
 
-
+#x轴是日期，31个。
 x = np.arange(1, 32,1)
-
+#设置所有图中的标记
 plt.title("Changes of temperature in 2019.3")
 plt.xlabel("Date")
 plt.ylabel("Temperature(high);Temperature(low)")
+#同时画两条图线
 plt.plot(x, high,"x-",label="Temperature(high)")
 plt.plot(x, low,"x-",label="Temperature(low)")
-
+#刻画出全部刻度
 plt.xticks(x)
 plt.grid(True)
 
